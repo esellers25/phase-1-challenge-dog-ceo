1b7ca5b4066f2ccd883821ff666805745ab56666
esellers25 <erin.e.sellers@gmail.com>
2021-03-22 14:43:15 -0400

    started work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+
+let dogImageContainer = document.querySelector("#dog-image-container")
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+
+    fetch("https://dog.ceo/api/breeds/image/random/4" )
+    .then(res => res.json())
+    .then(function(dogPictures) {
+        console.log(dogPictures)
+        dogPictures.forEach(function(dogPicture){
+        let dogImage = document.createElement("img")
+        dogImage.id = "dogImage"
+        dogImage.src = dogPicture.message
+        dogImageContainer.append(dogImage)
+        })
+    
+    })
+    
+
+
